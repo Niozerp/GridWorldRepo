737d89f7d82191b3975737563e3ceaf2cbfb1c0c
Jon Ausherman <1hizapper007@gmail.com>
2014-03-19 15:37:35 -0700

    All fixed up

--- JonAGridWorld/src/zorrobug/ZorroBugRunner.java
+++ JonAGridWorld/src/zorrobug/ZorroBugRunner.java
@@ -1,24 +1,23 @@
 /**
  * @author Jon Ausherman
  * @date Mar 19, 2014
- * @version 1.0
+ * @version 1.5
  */
 package zorrobug;
 
 import info.gridworld.actor.ActorWorld;
-import info.gridworld.grid.UnboundedGrid;
 
 public class ZorroBugRunner {
 
 	/**
-	 * @param args
+	 * @discription adds a ZorroBug object to the grid
+	 * I want to default set the grid to unbounded. Working on that
+	 * NO SUCCESS SO FAR
 	 */
 	public static void main(String[] args) {
 		ActorWorld world = new ActorWorld();
-		world.add(new ZorroBug(6));
-		world.setGrid(new UnboundedGrid());
-		world.show();
-
+        world.add(new ZorroBug(6));
+        world.show();
 	}
 
 }
